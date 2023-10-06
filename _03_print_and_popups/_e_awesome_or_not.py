1683596f21ddb3a020484325cdb6cb1a2fb087f0
league <gmjangwoo0827@gmail.com>
2023-10-06 16:28:33 -0700

    dddsda

--- _03_print_and_popups/_e_awesome_or_not.py
+++ _03_print_and_popups/_e_awesome_or_not.py
@@ -2,9 +2,9 @@ from tkinter import messagebox, simpledialog, Tk
 import random
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
 
     # 1. Make a variable equal to a positive number less than 4 using random.randInt(0, 3)
